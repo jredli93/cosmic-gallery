3e7e7e0fa564fe36f2cbc91b2d6cd74c2a1318bc
jovred <jovan.redli@namics.com>
2021-08-04 12:53:04 +0200

    artist strana

--- resources/views/pages/artists/content.blade.php
+++ resources/views/pages/artists/content.blade.php
@@ -1,6 +1,41 @@
 <section class="artists-content-wrap">
-    {{-- <h1 class="artists-title">Artists</h1> --}}
+    <h1 class="artists-title">Artists</h1>
     <div class="artists-content">
-
-    </div>   
+        <div class="single-artist">
+            <h1 class="single-artist-title">Darko Kovacevic</h1>
+            <div class="single-artist-wrap">
+                <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-left">
+                <div class="single-artist-right">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                </div>
+            </div>
+        </div>
+        <div class="single-artist">
+            <h1 class="single-artist-title">Darko Kovacevic</h1>
+            <div class="single-artist-wrap">
+                <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-left">
+                <div class="single-artist-right">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                </div>
+            </div>
+        </div>
+        <div class="single-artist">
+            <h1 class="single-artist-title">Darko Kovacevic</h1>
+            <div class="single-artist-wrap">
+                <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-left">
+                <div class="single-artist-right">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                    <img src="{{ asset('/assets/images/avatar.jpeg') }}" alt="" class="single-artist-image">
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
