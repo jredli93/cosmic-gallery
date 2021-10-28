ef50fa12f49b2e0f07921f3940f0af949bb584c3
Jovan Redli <>
2021-10-28 14:26:21 +0200

    artists grid

--- resources/views/pages/artists/content.blade.php
+++ resources/views/pages/artists/content.blade.php
@@ -1,5 +1,5 @@
 <section class="artists-content-wrap">
-    <span class="artists-overtitle">Our process of</span>
+    <span class="artists-overtitle">List</span>
     <h1 class="artists-title">Artists</h1>
 
     <h1 class="artist-name">Darko Kovacevic</h1>
@@ -10,7 +10,7 @@
         <img id="a1" src="{{ asset('/assets/images/piercing.jpg') }}" alt="" class="single-artist">
         <img id="a1" src="{{ asset('/assets/images/piercing.jpg') }}" alt="" class="single-artist">
     </div>
-    
+
     <h1 class="artist-name">Darko Kovacevic</h1>
     <div class="artists-content">
         <img id="a1" src="{{ asset('/assets/images/piercing.jpg') }}" alt="" class="single-artist image-grid-row-2 image-grid-col-2">
